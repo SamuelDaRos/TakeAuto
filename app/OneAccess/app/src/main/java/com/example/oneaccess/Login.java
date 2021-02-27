e0f8e1b7a25ddd14313bea173552b6c2d3fd3a4c
ale.casagrande <alessandro.casagrande.03@iisvittorioveneto.it>
2021-02-27 15:42:51 +0100

    creata struttura repository, messo file progetto all'interno di 'app'

---
+++ app/OneAccess/app/src/main/java/com/example/oneaccess/Login.java
@@ -0,0 +1,22 @@
+package com.example.oneaccess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = this.getIntent();
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
