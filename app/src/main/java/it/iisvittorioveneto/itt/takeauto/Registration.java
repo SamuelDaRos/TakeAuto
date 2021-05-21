f162471e4001036a0b04f418a60d3fb31eccc4bc
SamuelDaRos <samuel.daros@iisvittorioveneto.it>
2021-05-21 10:50:31 +0200

    aggiunto database, rifare controllo psw

--- app/src/main/java/it/iisvittorioveneto/itt/takeauto/Registration.java
+++ app/src/main/java/it/iisvittorioveneto/itt/takeauto/Registration.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,25 +16,28 @@ import com.example.takeauto.R;
 import java.time.LocalDate;
 
 public class Registration extends AppCompatActivity {
+    DatabaseHelper myDB;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
+        myDB = new DatabaseHelper(this);
         Intent intent = this.getIntent();
-        Button button= findViewById(R.id.buttonConfirm);
+        Button button = findViewById(R.id.buttonConfirm);
         button.setOnClickListener(v -> {
-            EditText email= findViewById(R.id.emailAddress);
-            EditText username= findViewById(R.id.usernameRegistration);
-            EditText password= findViewById(R.id.passwordRegistration);
-            EditText confirmPassword= findViewById(R.id.passwordRegistrationConfirm);
-            EditText dateOfBirth= findViewById(R.id.dateOfBirth);
-            EditText phoneNumber= findViewById(R.id.phone);
-            EditText fiscalCode= findViewById(R.id.fiscalCode);
-
-            if(password.getText().toString().equals(confirmPassword.getText().toString())) {
+            EditText email = findViewById(R.id.emailAddress);
+            EditText username = findViewById(R.id.usernameRegistration);
+            EditText password = findViewById(R.id.passwordRegistration);
+            EditText confirmPassword = findViewById(R.id.passwordRegistrationConfirm);
+            EditText dateOfBirth = findViewById(R.id.dateOfBirth);
+            EditText phoneNumber = findViewById(R.id.phone);
+            EditText fiscalCode = findViewById(R.id.fiscalCode);
+            addData();
+            //non funziona niente
+            /*
+            if (password.getText().toString().equals(confirmPassword.getText().toString())) {
                 User user = new User(
                         email.getText().toString(),
                         username.getText().toString(),
@@ -48,6 +52,29 @@ public class Registration extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "the 2 insered password don't match", Toast.LENGTH_LONG).show();
             }
+
+             */
         });
-    }
+        }
+
+        public void addData () {
+            Button button = findViewById(R.id.buttonConfirm);
+            button.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            EditText email = findViewById(R.id.emailAddress);
+                            EditText username = findViewById(R.id.usernameRegistration);
+                            EditText password = findViewById(R.id.passwordRegistration);
+                            boolean isInserted = myDB.insertDeta(username.getText().toString(), email.getText().toString(), password.getText().toString());
+                            if (isInserted == true) {
+                                Toast.makeText(Registration.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            } else
+                                Toast.makeText(Registration.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+            );
+        }
 }
+
