a007d35c1d9a1730d81b9bade7e0b9aa9f946f08
Alessandro <alessandro.casagrande.03@iisvittorioveneto.it>
2021-05-22 17:49:26 +0200

    eliminati attributi che causavano errori, per salvare un Utente, nella prima versione, va bene anche senza

--- app/src/main/java/it/iisvittorioveneto/itt/takeauto/Registration.java
+++ app/src/main/java/it/iisvittorioveneto/itt/takeauto/Registration.java
@@ -31,49 +31,41 @@ public class Registration extends AppCompatActivity {
             EditText username = findViewById(R.id.usernameRegistration);
             EditText password = findViewById(R.id.passwordRegistration);
             EditText confirmPassword = findViewById(R.id.passwordRegistrationConfirm);
-            EditText dateOfBirth = findViewById(R.id.dateOfBirth);
-            EditText phoneNumber = findViewById(R.id.phone);
-            EditText fiscalCode = findViewById(R.id.fiscalCode);
+            //EditText dateOfBirth = findViewById(R.id.dateOfBirth);
+            //EditText phoneNumber = findViewById(R.id.phone);
+            //EditText fiscalCode = findViewById(R.id.fiscalCode);
             addData();
-            //non funziona niente
-            /*
+
             if (password.getText().toString().equals(confirmPassword.getText().toString())) {
                 User user = new User(
                         email.getText().toString(),
                         username.getText().toString(),
-                        password.getText().toString(),
-                        LocalDate.parse(dateOfBirth.getText()), //non funziona
-                        phoneNumber.getText().toString(),
-                        fiscalCode.getText().toString()
+                        password.getText().toString()
+                        //LocalDate.parse(dateOfBirth.getText()),
+                        //phoneNumber.getText().toString(),
+                        //fiscalCode.getText().toString()
                 );
                 MainActivity.userList.add(user);
                 Intent finalIntent = new Intent(Registration.this, Final_Activity.class);
                 startActivity(finalIntent);
             } else {
-                Toast.makeText(this, "the 2 insered password don't match", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "the 2 inserted password don't match", Toast.LENGTH_LONG).show();
             }
-
-             */
         });
-        }
+    }
 
         public void addData () {
             Button button = findViewById(R.id.buttonConfirm);
-            button.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            EditText email = findViewById(R.id.emailAddress);
-                            EditText username = findViewById(R.id.usernameRegistration);
-                            EditText password = findViewById(R.id.passwordRegistration);
-                            boolean isInserted = myDB.insertDeta(username.getText().toString(), email.getText().toString(), password.getText().toString());
-                            if (isInserted == true) {
-                                Toast.makeText(Registration.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                            } else
-                                Toast.makeText(Registration.this, "Data not Inserted", Toast.LENGTH_LONG).show();
-
-                        }
-                    }
+            button.setOnClickListener(v -> {
+                EditText email = findViewById(R.id.emailAddress);
+                EditText username = findViewById(R.id.usernameRegistration);
+                EditText password = findViewById(R.id.passwordRegistration);
+                boolean isInserted = myDB.insertDeta(username.getText().toString(), email.getText().toString(), password.getText().toString());
+                if (isInserted) {
+                    Toast.makeText(Registration.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(Registration.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+            }
             );
         }
 }
