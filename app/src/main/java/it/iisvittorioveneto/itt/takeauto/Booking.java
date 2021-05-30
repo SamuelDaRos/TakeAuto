ddfef0f50f2335d6273d7f36fec5d0cbfcea92da
MarcoB043 <79756210+MarcoB043@users.noreply.github.com>
2021-05-30 16:00:12 +0200

    Tolto la parte dialog

--- app/src/main/java/it/iisvittorioveneto/itt/takeauto/Booking.java
+++ app/src/main/java/it/iisvittorioveneto/itt/takeauto/Booking.java
@@ -3,22 +3,22 @@ package it.iisvittorioveneto.itt.takeauto;
 import java.time.LocalDateTime;
 
 public class Booking {
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private String startDate;
+    private String endDate;
     private User user;
     private Car car;
     Login l= new Login();
 
-    public Booking(LocalDateTime startDate,LocalDateTime endDate, Car car) {
+    public Booking(String startDate,String endDate, Car car) {
         this.startDate=startDate;
         this.endDate = endDate;
         this.user = l.getActualUser();
         this.car = car;
     }
 
-    public LocalDateTime getStartDate() { return startDate; }
+    public String getStartDate() { return startDate; }
 
-    public LocalDateTime getEndDate() {
+    public String getEndDate() {
         return endDate;
     }
 
